2a67b777018982b9457d697273df3a81ebbc9496
ss-deep <deepalisangole@gmail.com>
2024-05-15 14:08:49 -0500

    connected to db

--- model.py
+++ model.py
@@ -1,11 +1,47 @@
+import os
+from dotenv import load_dotenv
 from flask_sqlalchemy import SQLAlchemy
 
+
 db=SQLAlchemy()
+load_dotenv()
+
+POSTGRES_URI=os.getenv("POSTGRES_URI")
+
+def connect_to_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["POSTGRES_URI"]
+    app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+    db.init_app(app)
 
 class User(db.Model):
     
-    __tablename__="user"
+    __tablename__="users"
 
-    id = db.column(db.Integer, primary_key=True,autoincrement=True)
-    username = db.Column(db.String(255), unitque=True, nullable=False)
-    password=db.column(db.String(255),nullable=False)
+    id = db.Column(db.Integer, primary_key = True,autoincrement=True)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    password=db.Column(db.String(255),nullable=False)
+
+class Team(db.Model):
+
+    __tablename__="teams"
+
+    id=db.Column(db.Integer, primary_key=True,autoincrement=True)
+    team_name=db.Column(db.String(255), unique=True,nullable=False)
+    user_id=db.Column(db.Integer, db.ForeignKey("user_id"), nullable=False)
+
+class Project(db.Model):
+
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    project_name = db.Column(db.String(255), nullable = False)
+    description = db.Column(db.String(255), nullable = True)
+    completed = db.Column(db.Boolean, default = False)
+    team_id = db.Column(db.Integer, db.ForeignKey("teams.id"), nullable = False)
+
+
+    if __name__=="__main__":
+        from flask import Flask
+        app=Flask(__name__)
+        connect_to_db(app)
+        print("Connected to db!")
