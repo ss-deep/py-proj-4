35a5280d22ea25731905d82bf74506e0aed1b72f
ss-deep <deepalisangole@gmail.com>
2024-05-17 20:06:59 -0500

    project form created

--- forms.py
+++ forms.py
@@ -1,11 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField
+from wtforms import StringField,SubmitField,TextAreaField,BooleanField,SelectField
 from wtforms.validators import DataRequired, Length
 # from markupsafe import Markup
 
 class TeamForm(FlaskForm):
 
-    team_name=StringField("team name",validators=[DataRequired(),Length(min=4,max=255)])
-    submit=SubmitField("submit")
+    team_name=StringField("Team Name",validators=[DataRequired(),Length(min=4,max=255)])
+    submit=SubmitField("Submit")
 
-    
+class ProjectForm(FlaskForm):
+
+    project_name=StringField("Project Name",validators=[DataRequired()])
+    description=TextAreaField("Description")
+    completed=BooleanField("Completed",false_values={False, 'false', ''})
+    team_id=SelectField("Team Id")
+    submit=SubmitField("Submit")
--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, url_for, redirect
-from forms import TeamForm
+from forms import TeamForm,ProjectForm
 
 app=Flask(__name__)
 app.secret_key = "keep this secret"
 
 @app.route("/")
 def home():
-    team_form=TeamForm()
-    return render_template("home.html", team_form=team_form)
+    # team_form=TeamForm()
+    return render_template("home.html")
 
 @app.route("/add_team", methods=["GET","POST"])
 def add_team():
@@ -18,7 +18,19 @@ def add_team():
     else: 
         print("Form failed to validate on submit.")
     
-    return redirect(url_for('home'))
+    return render_template("teams.html",team_form=team_form)
+
+@app.route('/add_project', methods=["GET","POST"])
+def add_project():
+    proj_form=ProjectForm()
+    if proj_form.validate_on_submit():
+        project_name=proj_form.project_name.data
+        print(project_name)
+    else:
+        print("project not found")
+
+    return render_template("projects.html",proj_form=proj_form)
+
 
 if __name__=="__main__":
     app.run(debug=True) 
